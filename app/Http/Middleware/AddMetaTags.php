4e3e421c2eba0af061700f138fd2bc5967a04aaa
Abdelmonaim oulaid <oulaidabdelmonaim@gmail.com>
2024-06-26 23:49:40 +0100

    changes

--- app/Http/Middleware/AddMetaTags.php
+++ app/Http/Middleware/AddMetaTags.php
@@ -20,7 +20,12 @@ class AddMetaTags
 
         if ($response instanceof \Illuminate\Http\Response) {
             $content = $response->getContent();
-            $metaTag = '<link rel="manifest" href="'.url('manifest.json').'" />';
+            $metaTag = '<link rel="manifest" href="'.url('manifest.json').'" />
+                <meta name="apple-mobile-web-app-capable" content="yes">
+                <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+                <meta name="apple-mobile-web-app-title" content="Your App Name">
+                <link rel="apple-touch-icon" href="'.url('img/icons/icon-192x192.png').'">
+                <link rel="apple-touch-startup-image" href="'.url('img/icons/icon-512x512.png').'">';
             $content = str_replace('<head>', "<head>\n{$metaTag}", $content);
             $response->setContent($content);
         }
